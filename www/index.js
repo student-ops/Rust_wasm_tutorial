a0c963e15d9b7a84800ba13164d61ea21d71d67d
stundet-ops <gogakuec@mail.com>
2023-07-06 15:39:22 +0900

    the game works fine !!

--- www/index.js
+++
@@ -1,4 +0,0 @@
-import * as wasm from "../pkg/wasm_game_of_life.js";
-
-wasm.greet("ryuta");
-
---
+++ www/my_sample.js
@@ -0,0 +1,32 @@
+import { Universe } from "wasm-game-of-life";
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+let lastRenderTime = Date.now();
+
+const renderLoop = () => {
+    const currentTime = Date.now();
+    const deltaTime = currentTime - lastRenderTime;
+
+    if (deltaTime >= 500) {
+        pre.textContent = universe.render();
+        universe.tick();
+        lastRenderTime = currentTime;
+    }
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
+// const pre = document.getElementById("game-of-life-canvas");
+// const universe = Universe.new();
+
+// const renderLoop = () => {
+//     pre.textContent = universe.render();
+//     universe.tick();
+// };
+
+// setInterval(renderLoop, 500);
+
